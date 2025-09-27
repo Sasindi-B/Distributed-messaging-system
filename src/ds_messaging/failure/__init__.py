e984537e344b1d1a57b754240ccdab6218302ade
Shadhu23 <it23749498@my.sliit.lk>
2025-09-27 05:55:43 +0530

    Data-replication-and-consistency part added

--- src/ds_messaging/failure/__init__.py
+++ src/ds_messaging/failure/__init__.py
@@ -1,6 +1,6 @@
 
 from .detector import FailureDetector
-from .replication import replicate_to_peers, replicate_with_quorum
+from ..failure.replication import replicate_to_peers, replicate_with_quorum
 from .heartbeat import heartbeat_task, rejoin_sync
 from .api import send_handler, replicate_handler, heartbeat_handler, sync_handler, messages_handler
 
